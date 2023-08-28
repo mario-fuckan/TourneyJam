d7f65bbd2fbb7994193f05a246e9fdd79387210a
Mario Fučkan <mario.fucchan15@gmail.com>
2023-08-28 15:38:55 +0200

    Finished top navigation search

--- src/lib/components/topNavigation.svelte
+++ src/lib/components/topNavigation.svelte
@@ -9,6 +9,7 @@
 	import dataToPass from "$lib/stores/navigationRefresh"
 	import Loading from "$lib/components/others/loading.svelte"
 	import { beforeNavigate } from "$app/navigation"
+	import Nocontent from "./others/nocontent.svelte"
 	import type { User as NavigationUser, Game, Tournament, Tag } from "$lib/types/navigationTypes"
 
 	let user: User
@@ -150,64 +151,80 @@
 						<div class="navbox">
 							<h1>Users</h1>
 							<hr />
-							<div class="navboxcontent">
-								{#each users as { username, profile_picture, badges }}
-									<a href="/profile/{username}">
-										<img src={profile_picture} alt={username} />
-										{username}
-										<div class="navbadges">
-											{#if badges.length > 0}
-												{#each badges as badge}
-													<Icon
-														icon={userbadges[badge].icon}
-														style="color: {userbadges[badge].color}"
-													/>
-												{/each}
-											{/if}
-										</div>
-									</a>
-								{/each}
-							</div>
+							{#if users.length > 0}
+								<div class="navboxcontent">
+									{#each users as { username, profile_picture, badges }}
+										<a href="/profile/{username}">
+											<img src={profile_picture} alt={username} />
+											{username}
+											<div class="navbadges">
+												{#if badges.length > 0}
+													{#each badges as badge}
+														<Icon
+															icon={userbadges[badge].icon}
+															style="color: {userbadges[badge].color}"
+														/>
+													{/each}
+												{/if}
+											</div>
+										</a>
+									{/each}
+								</div>
+							{:else}
+								<Nocontent missing="users" />
+							{/if}
 						</div>
 						<div class="navbox">
 							<h1>Games</h1>
 							<hr />
-							<div class="navboxcontent">
-								{#each games as { id, game_name, game_cover }}
-									<a href="/games/{id}">
-										<img src={game_cover} alt={game_name} />
-										{game_name}
-									</a>
-								{/each}
-							</div>
+							{#if games.length > 0}
+								<div class="navboxcontent">
+									{#each games as { id, game_name, game_cover }}
+										<a href="/games/{id}">
+											<img src={game_cover} alt={game_name} />
+											{game_name}
+										</a>
+									{/each}
+								</div>
+							{:else}
+								<Nocontent missing="games" />
+							{/if}
 						</div>
 						<div class="navbox">
 							<h1>Tournaments</h1>
 							<hr />
-							<div class="navboxcontent">
-								{#each tournaments as { id, title, cover_image, authUserId, players }}
-									<a href="/tournaments/{id}">
-										<img src={cover_image} alt={title} />
-										{title}
-										{#if authUserId == user?.userId}
-											<div class="tournamenttag">Created</div>
-										{:else if checkIfJoined(players)}
-											<div class="tournamenttag">Joined</div>
-										{/if}
-									</a>
-								{/each}
-							</div>
+							{#if tournaments.length > 0}
+								<div class="navboxcontent">
+									{#each tournaments as { id, title, cover_image, authUserId, players }}
+										<a href="/tournaments/{id}">
+											<img src={cover_image} alt={title} />
+											{title}
+											{#if authUserId == user?.userId}
+												<div class="tournamenttag">Created</div>
+											{:else if checkIfJoined(players)}
+												<div class="tournamenttag">Joined</div>
+											{/if}
+										</a>
+									{/each}
+								</div>
+							{:else}
+								<Nocontent missing="tournaments" />
+							{/if}
 						</div>
 						<div class="navbox">
 							<h1>Tags</h1>
 							<hr />
-							<div class="navboxcontent">
-								{#each tags as { tag }}
-									<a href="/tag/{tag}">
-										<p>{tag}</p>
-									</a>
-								{/each}
-							</div>
+							{#if tags.length > 0}
+								<div class="navboxcontent">
+									{#each tags as { tag }}
+										<a href="/tag/{tag}">
+											<p>{tag}</p>
+										</a>
+									{/each}
+								</div>
+							{:else}
+								<Nocontent missing="tags" />
+							{/if}
 						</div>
 					</div>
 				{/if}
